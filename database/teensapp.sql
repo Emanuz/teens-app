d02b8208d851352d6dfd0e1679a1936919a7f719
Ggero Mpyangu <nanobotg@gmail.com>
2017-02-11 12:36:38 +0300

    Database SQL
    Added an SQL file containing database info

---
+++ database/teensapp.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 11, 2017 at 10:32 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `teensapp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+-- in use(#1932 - Table 'teensapp.comments' doesn't exist in engine)
+-- Error reading data: (#1932 - Table 'teensapp.comments' doesn't exist in engine)
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts_devotions`
+--
+-- in use(#1932 - Table 'teensapp.posts_devotions' doesn't exist in engine)
+-- Error reading data: (#1932 - Table 'teensapp.posts_devotions' doesn't exist in engine)
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts_lifeskills`
+--
+-- in use(#1932 - Table 'teensapp.posts_lifeskills' doesn't exist in engine)
+-- Error reading data: (#1932 - Table 'teensapp.posts_lifeskills' doesn't exist in engine)
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts_testimonies`
+--
+-- in use(#1932 - Table 'teensapp.posts_testimonies' doesn't exist in engine)
+-- Error reading data: (#1932 - Table 'teensapp.posts_testimonies' doesn't exist in engine)
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_accounts`
+--
+-- in use(#1932 - Table 'teensapp.user_accounts' doesn't exist in engine)
+-- Error reading data: (#1932 - Table 'teensapp.user_accounts' doesn't exist in engine)
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
