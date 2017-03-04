8f8ef0ed19ae465f88f115769358927ce560f638
Ggero Mpyangu <nanobotg@gmail.com>
2017-03-04 12:52:13 +0300

    Update Database
    Support for comments
    Support for categories

--- database/teensapp.sql
+++ database/teensapp.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 11, 2017 at 10:32 AM
+-- Generation Time: Mar 04, 2017 at 10:45 AM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 5.6.28
 
@@ -23,43 +23,132 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `category_id` int(10) NOT NULL,
+  `category_name` text NOT NULL,
+  `category_slug` text NOT NULL,
+  `category_group` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`category_id`, `category_name`, `category_slug`, `category_group`) VALUES
+(1, 'Life Skills', 'lifeskills', 0),
+(2, 'Testimonies', 'testimonies', 0),
+(3, 'Devotions', 'devotions', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `comments`
 --
--- in use(#1932 - Table 'teensapp.comments' doesn't exist in engine)
--- Error reading data: (#1932 - Table 'teensapp.comments' doesn't exist in engine)
+
+CREATE TABLE `comments` (
+  `comment_id` int(10) NOT NULL,
+  `post_id` int(10) NOT NULL,
+  `comment_author` int(10) NOT NULL,
+  `comment_date` datetime NOT NULL,
+  `comment_date_gmt` datetime NOT NULL,
+  `comment_content` text NOT NULL,
+  `comment_likes` int(11) NOT NULL,
+  `comment_approved` int(1) NOT NULL,
+  `comment_parent` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `posts_devotions`
+-- Table structure for table `postmeta`
 --
--- in use(#1932 - Table 'teensapp.posts_devotions' doesn't exist in engine)
--- Error reading data: (#1932 - Table 'teensapp.posts_devotions' doesn't exist in engine)
+
+CREATE TABLE `postmeta` (
+  `post_id` int(10) NOT NULL,
+  `category_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `posts_lifeskills`
+-- Table structure for table `posts`
 --
--- in use(#1932 - Table 'teensapp.posts_lifeskills' doesn't exist in engine)
--- Error reading data: (#1932 - Table 'teensapp.posts_lifeskills' doesn't exist in engine)
+
+CREATE TABLE `posts` (
+  `post_id` int(10) NOT NULL,
+  `post_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `post_date_gmt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `post_author` int(10) NOT NULL,
+  `post_content` text,
+  `post_title` text NOT NULL,
+  `post_excerpt` text,
+  `post_status` int(1) NOT NULL,
+  `post_comment_status` int(1) NOT NULL,
+  `post_date_modified` datetime DEFAULT CURRENT_TIMESTAMP,
+  `post_date_modified_gmt` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `posts_testimonies`
+-- Table structure for table `users`
 --
--- in use(#1932 - Table 'teensapp.posts_testimonies' doesn't exist in engine)
--- Error reading data: (#1932 - Table 'teensapp.posts_testimonies' doesn't exist in engine)
 
--- --------------------------------------------------------
+CREATE TABLE `users` (
+  `user_id` int(10) NOT NULL,
+  `user_pass` varchar(60) DEFAULT NULL,
+  `user_country_code` int(3) NOT NULL,
+  `user_phone` int(12) NOT NULL,
+  `user_name` text NOT NULL,
+  `user_email` text,
+  `user_url` text,
+  `user_date_registered` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Table structure for table `user_accounts`
+-- Indexes for dumped tables
 --
--- in use(#1932 - Table 'teensapp.user_accounts' doesn't exist in engine)
--- Error reading data: (#1932 - Table 'teensapp.user_accounts' doesn't exist in engine)
 
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`post_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `category_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comment_id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `post_id` int(10) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
